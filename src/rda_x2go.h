685e6c3f471c226b43464212238731c7a86dbe99
Mihai Moldovan <ionic@ionic.de>
2023-07-20 02:54:58 +0200

    src/rda_x2go.{c,h}: add generic protocol support.
    X2Go supports both NX and KDrive sessions, so parse the session ID
    (X2GO_SESSION) to find out what protocol the session uses.

--- src/rda_x2go.h
+++ src/rda_x2go.h
@@ -27,8 +27,14 @@
 #include <glib.h>
 
 #include <rda.h>
+#include <rda_protocol.h>
 
 #ifdef WITH_REMOTE_AWARENESS_X2GO
+
+extern const rda_protocol_t
+rda_supported_protocols_x2go[];
+extern const gsize
+rda_supported_protocols_x2go_len;
 
 gboolean
 rda_session_is_x2go (void);
